181f41e47242d8f4af3ed0af639a6a8029326f7e
Konrad <konradgaik@icloud.com>
2021-04-13 23:35:28 +0200

    regural expresions added

--- add.php
+++ add.php
@@ -1,10 +1,30 @@
 <?php
 
 if(isset($_POST['submit'])){
-echo $_POST['email'].'<br />';
-echo $_POST['name'].'<br />';
-echo $_POST['title'].'<br />';
-echo $_POST['ingredients'].'<br />';
+
+if(empty($_POST['email'])){
+	echo "Nie podales e-maila.</br>";
+}else{$email = $_POST['email'] ;
+if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+echo 'Email nieprawidlowy.';}
+}
+
+if(empty($_POST['name'])){echo "Nie podales imienia.";}
+else{$name = $_POST['name'];
+	if (!(preg_match('/^[a-zA-Z\s]+$/',$name)));
+	else{echo 'Podano niepoprawne imie.';}}
+
+if(empty($_POST['title'])){
+	echo "Nie podales nazwy pizzy";}
+	else{$title = $_POST['title'];
+		if (!(preg_match('/^[a-zA-Z\s]+$/',$title)));
+		else{echo 'Podano niepoprawna nazwe pizzy.';}}
+
+if(empty($_POST['dodatki'])){
+	echo "Nie podales dodatkow";
+}else{$dodatki = $_POST['dodatki'];
+	if (!(preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/',$dodatki)));
+	else{echo 'Podano niepoprawny format dodatkow.';}}
 }
 ?>
 <!DOCTYPE html>
@@ -20,7 +40,7 @@ echo $_POST['ingredients'].'<br />';
             <label>Nazwa pizzy</label>
 			<input type="text" name="title">
 			<label>Dodatki(oddzielone przecinkami) </label>
-			<input type="text" name="ingredients">
+			<input type="text" name="dodatki">
 			<div class="center">
 				<input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
 			</div>
