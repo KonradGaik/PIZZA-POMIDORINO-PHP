bf5ed51b71b93a36f612e97faaa82d6653a8896e
Konrad <konradgaik@icloud.com>
2021-04-14 20:37:22 +0200

    Login system added

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
+session_start();
+if(!empty($_SESSION['user']) :   ?>
 
-?>
 <!DOCTYPE html>
 <html lang="pl">
-    
+ 
 <?php include ('templates/header.php') ?>
 
 <h1>Witamy, witamy, witamy</h1>
 
+<p> Hi <?php else :?> </p>   
+    <?php  elseif: ?> 
+<a href="logout.php">Wyloguj</a>
 <?php include ('templates/footer.php') ?>
+<?php endif; ?>
 </html>
--- login.php
+++ login.php
@@ -1,17 +1,16 @@
 <?php 
-
 require_once ('db.php');
 session_start();
-$login = '';
-$password = '';
+
 if(!empty($_POST['login']) && !empty($_POST['password'])){
-if($_POST['login']==USERNAME){
- password_hash(PASSWORD,PASSWORD_DEFAULT);
+if($_POST['login'] == USERNAME){
  if(password_verify($_POST['password'],PASSWORD)){
-echo 'Poprawnie zalogowane';
- }else{echo 'Niepoprawnie zalogowano';}
+$_SESSION['user'] = strip_tags($_POST['login']);
+header('Location:http://192.168.64.2/THENINJANETPHP/index.php ');
+ }else{echo 'Niepoprawne hasło lub login.';}
 }
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="pl">
@@ -20,10 +19,11 @@ echo 'Poprawnie zalogowane';
 
 <h1>Logowanie</h1>
 <form action="login.php" method="post"></form>
-<h2>Login:</h2>
-<input type="text" name="login" value="<?php echo htmlspecialchars($login) ?>">
-<h2>Hasło:</h2>
-<input type="text" name="password" value="<?php echo htmlspecialchars($password) ?>">
+<h4>Login:</h4>
+<input type="text" name="login" >
+<h4>Hasło:</h4>
+<input type="text" name="password" >
 <input type="submit" name="submit" value="Potwierdz" class="btn brand z-depth-0">
-<?php include ('templates/footer.php') ?>
+<?php include ('templates/footer.php');  ?>
 </html>
+
