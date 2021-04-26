4bdf714950326cbc537a375bdf960afa8aebac3a
Konrad <konradgaik@icloud.com>
2021-04-26 22:09:49 +0200

    database

--- config/connect_db.php
+++ config/connect_db.php
@@ -1,4 +1,41 @@
-<?php $conn = mysqli_connect('localhost','Konrad','test1234','pomidorino_pizza');
-if(!$conn){
-echo 'Blad polaczenia:'. mysqli_connect_error();} ?>
+// $conn = mysqli_connect('localhost','Konrad','test1234','pomidorino_pizza');
+// if(!$conn){
+// echo 'Blad polaczenia:'. mysqli_connect_error();}
 
+
+<?php
+ 
+ $database = new Medoo([
+	// required
+	'database_type' => 'mysql',
+	'database_name' => 'name',
+	'server' => 'localhost',
+	'username' => 'your_username',
+	'password' => 'XD',
+ 
+	// [optional]
+	'charset' => 'utf8mb4',
+	'collation' => 'utf8mb4_general_ci',
+	'port' => 3306,
+ 
+	// [optional] Table prefix
+	'prefix' => 'PREFIX_',
+ 
+	// [optional] Enable logging (Logging is disabled by default for better performance)
+	'logging' => true,
+ 
+	// [optional] MySQL socket (shouldn't be used with server and port)
+	'socket' => '/tmp/mysql.sock',
+ 
+	'option' => [
+        PDO::ATTR_CASE => PDO::CASE_NATURAL, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION 
+	]
+]);
+ 
+$database->insert("account", [
+	"user_name" => "localhost",
+	"email" => "foo@bar.com"
+]);
+
+
+?>
